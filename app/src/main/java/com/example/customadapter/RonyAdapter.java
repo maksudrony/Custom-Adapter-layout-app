7b99ab03fca3fedce56170b830bf9185ff9baca1
maksudrony <maksudrony44@gmail.com>
2022-06-06 17:39:46 +0600

    custom Adapter App

--- app/src/main/java/com/example/customadapter/RonyAdapter.java
+++ app/src/main/java/com/example/customadapter/RonyAdapter.java
@@ -6,14 +6,17 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 public class RonyAdapter extends ArrayAdapter<String> {
+    private Context context;
     private String [] arr;
     public RonyAdapter(@NonNull Context context, int resource, @NonNull String[] arr) {
         super(context, resource, arr);
+        this.context= context;
         this.arr= arr;
     }
 
@@ -29,6 +32,13 @@ public class RonyAdapter extends ArrayAdapter<String> {
         convertView = LayoutInflater.from(getContext()).inflate(R.layout.my_rony_layout, parent, false);
         TextView t= convertView.findViewById(R.id.textView);
         t.setText(getItem(position));
+        //making onclickListener in custom layout
+        convertView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, "You clicked on " + position, Toast.LENGTH_SHORT).show();
+            }
+        });
 
         return convertView;
     }
